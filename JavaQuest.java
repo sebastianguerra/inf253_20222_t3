5c3e85c42e366dc55536c49c665c9d9452f3ba5c
sebastianguerra <spguerraespinoza@gmail.com>
2022-10-19 21:11:53 -0300

    devolvi visualizacion a como estaba

--- JavaQuest.java
+++ JavaQuest.java
@@ -4,10 +4,10 @@ import java.util.SortedSet;
 
 public class JavaQuest {
     public static void main(String[] args) {
-        SortedSet<Edge> edges = GraphGenerator.Generar(5, 50);
+        SortedSet<Edge> edges = GraphGenerator.Generar(5);
         int i = 0;
         for (Edge e : edges) {
-            System.out.printf("%d: (%d, %d)\n", i++, e.x, e.y);
+            System.out.printf("(%d) -> (%d)\n", e.x, e.y);
         }
     }
 }
