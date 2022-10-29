48e1c7cd53d5b7776a5d1206e81056dabd9c26ba
sebastianguerra <spguerraespinoza@gmail.com>
2022-10-28 21:21:59 -0300

    implemente combate

--- Code/Personaje.java
+++ Code/Personaje.java
@@ -5,10 +5,10 @@ import java.util.Random;
 public class Personaje {
     private String nombre; // Nombre del personaje.
     private Integer dinero; // Cuanto dinero tiene el personaje.
-    private Integer hp_actual; // Cantidad de vida que tiene actualmente el personaje.
-    private Integer hp_total; // Cantidad de vida maxima que puede tener el personaje.
-    private Integer danio; // Cantidad de vida que le resta a su oponente al combatir (antes de aplicar defensa).
-    private Integer defensa; // Cantidad de danio evitado por golpe al combatir.
+    protected Integer hp_actual; // Cantidad de vida que tiene actualmente el personaje.
+    protected Integer hp_total; // Cantidad de vida maxima que puede tener el personaje.
+    protected Integer danio; // Cantidad de vida que le resta a su oponente al combatir (antes de aplicar defensa).
+    protected Integer defensa; // Cantidad de danio evitado por golpe al combatir.
 
     private Random rand;
 
@@ -26,6 +26,26 @@ public class Personaje {
 
     public void combate(Personaje enemigo) {
         /* Realiza el combate entre dos personajes. Durante un combate se selecciona un personaje aleatoriamente para comenzar y posteriormente se alternan al atacar. Durante un ataque, el personaje defensor pierde vida equivalente al danio de su oponente menos la defensa del defensor. El combate termina un vez la hp_actual de uno de los personajes sea menor o igual a 0. */
-        // TODO
+        Boolean turnoEnemigo = rand.nextBoolean();
+
+        while (enemigo.stillAlive() && this.stillAlive()) {
+            if (turnoEnemigo)
+                this.recibirAtaque(enemigo.getDanio());
+            else
+                enemigo.recibirAtaque(this.getDanio());
+        }
+
+    }
+
+    public void recibirAtaque(int danio) {
+        this.hp_actual -= danio - this.defensa;
+    }
+
+    public int getDanio(){
+        return this.danio;
+    }
+
+    public boolean stillAlive(){
+        return this.hp_actual>0;
     }
 }
