e6826a2cee027a0deccbed471244d4e87cd4c54d
sebastianguerra <spguerraespinoza@gmail.com>
2022-11-02 20:29:40 -0300

    Ahora se puede comprar mas de un item

--- Code/Nodos/NodoTienda.java
+++ Code/Nodos/NodoTienda.java
@@ -25,38 +25,36 @@ public class NodoTienda extends Nodo {
         /* Le aplica el item en la posicion indicada al jugador. */
         jugador.aplicarItem(inventario.get(pos));
         jugador.cobrar(inventario.get(pos).getPrecio());
+        inventario.remove(pos.intValue());
     }
     public void interactuar(Jugador jugador) {
         // TODO poder comprar mas que un item
         /* Le muestra los items en el inventario de la tienda al usuario y cuanto dinero tiene a su disposicion. Le permite comprar tantos items como quiera. */
         System.out.println("Bienvenido a la tienda!");
         // System.out.println("Tienes "+jugador.getDinero()+" dinero.");
-        jugador.verEstado();
-        System.out.println("Inventario:");
-        System.out.println("-----------------------------------");
-        for (int i = 0; i < inventario.size(); i++) {
-            System.out.printf("%d:\n", i+1);
-            inventario.get(i).showInfo();
+        int eleccion;
+        do {
+            jugador.verEstado();
+            System.out.println("Inventario:");
             System.out.println("-----------------------------------");
-        }
-        System.out.println("Que deseas comprar? (Ingresa el numero del item que deseas comprar, 0 para omitir)");
-        int eleccion = sc.nextInt();
-        boolean eleccionValida = false;
-        while(!eleccionValida) {
-            if ( eleccion == 0 ) {
-                eleccionValida = true;
-            } else if ( eleccion > 0 && eleccion <= inventario.size() ) {
+            for (int i = 0; i < inventario.size(); i++) {
+                System.out.printf("%d:\n", i+1);
+                inventario.get(i).showInfo();
+                System.out.println("-----------------------------------");
+            }
+            System.out.println("Que deseas comprar? (Ingresa el numero del item que deseas comprar, 0 para omitir)");
+            eleccion = sc.nextInt();
+            System.out.printf("eleccion: %d, Tamanio inventario: %d\n", eleccion, inventario.size());
+            if ( eleccion > 0 && eleccion <= inventario.size() ) {
                 if ( jugador.getDinero() >= inventario.get(eleccion-1).getPrecio() ) {
                     comprar(eleccion-1, jugador);
-                    eleccionValida = true;
                 } else {
-                    System.out.println("No tienes suficiente dinero para comprar este item.");
-                    eleccion = sc.nextInt();
+                    System.out.println("No tienes suficiente dinero para comprar este item.\nPrueba con otro.");
                 }
-            } else {
-                System.out.println("Opcion invalida.");
-                eleccion = sc.nextInt();
+            } else if (eleccion != 0) {
+                System.out.println("Opcion invalida.\n Intenta de nuevo");
             }
-        }
+            if (inventario.size() == 0) break;
+        } while(eleccion != 0);
     }
 }
